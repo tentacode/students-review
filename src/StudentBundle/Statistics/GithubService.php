860d505fdbd96bda894390897eef7aae08107d1b
tentacode <contact@gabrielpillet.com>
2017-10-16 17:02:56 +0200

    Service container

--- src/StudentBundle/Statistics/GithubService.php
+++ src/StudentBundle/Statistics/GithubService.php
@@ -3,20 +3,68 @@
 namespace StudentBundle\Statistics;
 
 use StudentBundle\Entity\Team;
+use Doctrine\ORM\EntityManager;
 
 class GithubService
 {
+    private $em;
+    private $clientId;
+    private $clientSecret;
+    
+    public function __construct(EntityManager $em, string $clientId, string $clientSecret)
+    {
+        $this->em = $em;
+        $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
+    }
+    
     public function getStats(int $teamId): array
     {
-        $teamRepository = $em->getRepository(Team::class);
+        $teamRepository = $this->em->getRepository(Team::class);
         $githubUrl = $teamRepository->find($teamId)
             ->getGithubRepository()
         ;
         
-        return [
-            'commits' => 123,
-            'additions' => 123124,
-            'deletions' => 3298,
+        if (!preg_match('/([^\/]+)\/([^\/]+)$/', $githubUrl, $matches)) {
+            throw new \Exception('Url github invalide.');
+        }
+        
+        $user = $matches[1];
+        $repositoryName = $matches[2];
+        $clientId = $this->clientId;
+        $clientSecret = $this->clientSecret;
+        
+        $apiUrl = sprintf(
+            'https://api.github.com/repos/%s/%s/stats/contributors?client_id=%s&client_secret=%s',
+            $user,
+            $repositoryName,
+            $clientId,
+            $clientSecret
+        );
+        
+        $client = new \GuzzleHttp\Client();
+        $json = $client->request('GET', $apiUrl)
+            ->getBody()
+            ->getContents()
+        ;
+        
+        $data = json_decode($json, true);
+        
+        $stats = [
+            'commits' => 0,
+            'additions' => 0,
+            'deletions' => 0,
         ];
+        
+        foreach ($data as $contributor) {
+            $stats['commits'] += $contributor['total'];
+            
+            foreach ($contributor['weeks'] as $week) {
+                $stats['additions'] += $week['a'];
+                $stats['deletions'] += $week['d'];
+            }
+        }
+        
+        return $stats;
     }
 }
