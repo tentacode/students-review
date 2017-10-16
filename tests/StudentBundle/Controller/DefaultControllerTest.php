923a3b0582fa4de92b788eedc18e46a3cf4a6bb4
tentacode <contact@gabrielpillet.com>
2017-10-16 18:00:23 +0200

    Tests phpunit

--- tests/StudentBundle/Controller/DefaultControllerTest.php
+++ tests/StudentBundle/Controller/DefaultControllerTest.php
@@ -6,12 +6,48 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testTeamList()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $this->assertContains('Test Étudiants', $client->getResponse()->getContent());
+    }
+    
+    public function testTeamHeroes()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/projet/1');
+
+        $this->assertContains('Villains', $client->getResponse()->getContent());
+    }
+    
+    public function testConnexion()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+        
+        $link = $crawler
+            ->filter('a:contains("Se connecter")')
+            ->eq(0)
+            ->link()
+        ;
+
+        $crawler = $client->click($link);
+        
+        $this->assertContains('Connexion', $client->getResponse()->getContent());
+        
+        $link = $crawler
+            ->filter('a:contains("Créer un compte")')
+            ->eq(0)
+            ->link()
+        ;
+
+        $crawler = $client->click($link);
+        
+        $this->assertContains('Inscription', $client->getResponse()->getContent());
     }
 }
